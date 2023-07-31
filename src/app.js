0052b71622bdfe04b0e1ad68705f40c7dfc7986a
Kartik Patel <kartikpatel0170@gmail.com>
2023-07-31 14:59:18 -0600

    added passport config

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const session = require("express-session");
 const routes = require('./routes/routes');
 
 const app = express();
+const jwt_secret = process.env.JWT_SECRET;
 
 // parse json request body
 app.use(express.json());
+app.use(session({ secret: jwt_secret, saveUninitialized: true, resave: true }));
 
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +22,15 @@ const corsOpts = {
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
 };
 app.use(cors(corsOpts));
+
+// parse application/x-www-form-urlencoded
+// for easier testing with Postman or plain HTML forms
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// parse application/json
+app.use(bodyParser.json())
 
 // routes
 app.use('/api', routes);
