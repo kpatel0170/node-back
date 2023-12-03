ba3e07c4c208bc999a8df17d35d200ad9f5458c6
kpatel0170 <kartikpatel0170@gmail.com>
2023-12-03 14:00:33 -0600

    Config: google OAuth 2.0 config

---
+++ src/config/passport/googleStrategy.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const logger = require('../logger');
+
+const User = require('../../models/user');
+
+const serverUrl = process.env.NODE_ENV === 'production' ? process.env.SERVER_URL_PROD : process.env.SERVER_URL_DEV;
+
+const googleLogin = new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `${serverUrl}${process.env.GOOGLE_CALLBACK_URL}`,
+    proxy: true,
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    // console.log(profile);
+    try {
+      const oldUser = await User.findOne({ email: profile.email });
+
+      if (oldUser) {
+        return done(null, oldUser);
+      }
+    } catch (err) {
+      logger.error(err);
+    }
+
+    try {
+      const newUser = await new User({
+        googleId: profile.id,
+        email: profile.email,
+        name: profile.displayName,
+      }).save();
+      done(null, newUser);
+    } catch (err) {
+      logger.error(err);
+    }
+  },
+);
+
+module.exports = (passport) => {
+  passport.use(googleLogin);
+};
