d53aa3cfb14f7b87d9d905f4af4a38e4779e1e69
kpatel0170 <kartikpatel0170@gmail.com>
2023-12-03 13:59:45 -0600

    Config: passport-jwt strategy

---
+++ src/config/passport/jwtStrategy.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+const User = require('../../models/user');
+
+const isProduction = process.env.NODE_ENV === 'production';
+const secretOrKey = isProduction ? process.env.JWT_SECRET_PROD : process.env.JWT_SECRET_DEV;
+
+const jwtLogin = new JwtStrategy(
+  {
+    jwtFromRequest: ExtractJwt.fromHeader('x-auth-token'),
+    secretOrKey,
+  },
+  async (payload, done) => {
+    try {
+      const user = await User.findOne({ email: payload.email });
+
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    } catch (error) {
+      done(error, false);
+    }
+  },
+);
+
+module.exports = (passport) => {
+  passport.use(jwtLogin);
+};
