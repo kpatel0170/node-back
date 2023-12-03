61d014bae41de60e5866277752dae3b4868f0f24
kpatel0170 <kartikpatel0170@gmail.com>
2023-12-03 14:04:15 -0600

    Feat: Add generateJWT and toJSON methods to user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
+const jwt = require('jsonwebtoken');
+// const { join } = require('path');
 
 const { Schema } = mongoose;
 const roles = ['user', 'admin'];
@@ -26,6 +28,12 @@ const userSchema = new Schema(
     services: {
       facebook: String,
       google: String,
+    },
+    // google
+    googleId: {
+      type: String,
+      unique: true,
+      sparse: true,
     },
     role: {
       type: String,
@@ -60,25 +68,65 @@ userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, user.password).catch(() => false);
 };
 
-userSchema.statics = {
-  roles,
+userSchema.methods.toJSON = function () {
+  // const absoluteAvatarFilePath = `${join(__dirname, '../..', IMAGES_FOLDER_PATH)}${this.avatar}`;
+  // const avatar = isValidUrl(this.avatar)
+  //   ? this.avatar
+  //   : fs.existsSync(absoluteAvatarFilePath)
+  //   ? `${IMAGES_FOLDER_PATH}${this.avatar}`
+  //   : `${IMAGES_FOLDER_PATH}avatar2.jpg`;
 
-  async oAuthLogin({ service, id, email, name, picture }) {
-    const user = await this.findOne({ $or: [{ [`services.${service}`]: id }, { email }] });
-    if (user) {
-      user.services[service] = id;
-      if (!user.name) user.name = name;
-      return user.save();
-    }
-    const password = uuidv4();
-    return this.create({
-      services: { [service]: id },
-      email,
-      password,
-      name,
-    });
-  },
+  // return {
+  //   id: this._id,
+  //   provider: this.provider,
+  //   email: this.email,
+  //   username: this.username,
+  //   avatar: avatar,
+  //   name: this.name,
+  //   role: this.role,
+  //   createdAt: this.createdAt,
+  //   updatedAt: this.updatedAt,
+  // };
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
 };
+
+const isProduction = process.env.NODE_ENV === 'production';
+const secretOrKey = isProduction ? process.env.JWT_SECRET_PROD : process.env.JWT_SECRET_DEV;
+
+userSchema.methods.generateJWT = function () {
+  const token = jwt.sign(
+    {
+      expiresIn: '12h',
+      id: this._id,
+      provider: this.provider,
+      email: this.email,
+    },
+    secretOrKey,
+  );
+  return token;
+};
+
+// userSchema.statics = {
+//   roles,
+
+//   async oAuthLogin({ service, id, email, name, picture }) {
+//     const user = await this.findOne({ $or: [{ [`services.${service}`]: id }, { email }] });
+//     if (user) {
+//       user.services[service] = id;
+//       if (!user.name) user.name = name;
+//       return user.save();
+//     }
+//     const password = uuidv4();
+//     return this.create({
+//       services: { [service]: id },
+//       email,
+//       password,
+//       name,
+//     });
+//   },
+// };
 
 const UserModel = mongoose.model('User', userSchema);
 
