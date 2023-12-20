7ed411704675d5a86135e0a408925d89be779d5c
kpatel0170 <kartikpatel0170@gmail.com>
2023-12-20 00:48:13 -0600

    update: add role-based auth middleware and utils

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -2,26 +2,94 @@ const Joi = require("joi");
 
 const register = {
   body: Joi.object().keys({
-    name: Joi.string().required(),
+    name: Joi.string().required().label("Name"),
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8).max(128).messages({
-      "string.min": "Password must contain at least 8 characters",
-      "string.max": "Password must contain at most 128 characters"
-    })
+    password: Joi.string()
+      .label("Password")
+      .min(8)
+      .max(128)
+      .pattern(/^(?=.*[a-zA-Z])(?=.*\d).*$/, "password")
+      .message({
+        "string.base": "'{{#label}}' must be a string",
+        "string.min": "'{{#label}}' must be at least 8 characters",
+        "string.max": "'{{#label}}' must contain at most 128 characters",
+        "string.pattern.name": "password",
+        "string.pattern.base":
+          "'{{#label}}' must contain at least 1 letter and 1 number"
+      })
   })
 };
 
 const login = {
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8).max(128).messages({
-      "string.min": "Password must contain at least 8 characters",
-      "string.max": "Password must contain at most 128 characters"
-    })
+    password: Joi.string()
+      .label("Password")
+      .min(8)
+      .max(128)
+      .pattern(/^(?=.*[a-zA-Z])(?=.*\d).*$/, "password")
+      .message({
+        "string.base": "'{{#label}}' must be a string",
+        "string.min": "'{{#label}}' must be at least 8 characters",
+        "string.max": "'{{#label}}' must contain at most 128 characters",
+        "string.pattern.name": "password",
+        "string.pattern.base":
+          "'{{#label}}' must contain at least 1 letter and 1 number"
+      })
+  })
+};
+
+const logout = {
+  body: Joi.object().keys({
+    refreshToken: Joi.string().required()
+  })
+};
+
+const refreshTokens = {
+  body: Joi.object().keys({
+    refreshToken: Joi.string().required()
+  })
+};
+
+const forgotPassword = {
+  body: Joi.object().keys({
+    email: Joi.string().email().required()
+  })
+};
+
+const resetPassword = {
+  query: Joi.object().keys({
+    token: Joi.string().required()
+  }),
+  body: Joi.object().keys({
+    password: Joi.string()
+      .label("Password")
+      .min(8)
+      .max(128)
+      .pattern(/^(?=.*[a-zA-Z])(?=.*\d).*$/, "password")
+      .message({
+        "string.base": "'{{#label}}' must be a string",
+        "string.min": "'{{#label}}' must be at least 8 characters",
+        "string.max": "'{{#label}}' must contain at most 128 characters",
+        "string.pattern.name": "password",
+        "string.pattern.base":
+          "'{{#label}}' must contain at least 1 letter and 1 number"
+      })
+  })
+};
+
+const verifyEmail = {
+  query: Joi.object().keys({
+    token: Joi.string().required()
   })
 };
 
 module.exports = {
   register,
-  login
+  login,
+  logout,
+  refreshTokens,
+  forgotPassword,
+  resetPassword,
+  verifyEmail
 };
