f104c5ea038cdaef7d0244d7dc696ccf0c335952
kpatel0170 <kartikpatel0170@gmail.com>
2023-12-20 00:49:21 -0600

    update: user routes

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,99 +1,44 @@
-const userService = require("../service/user");
 const logger = require("../config/logger");
+const userService = require("../service/userService");
+const catchAsync = require("../utils/catchAsync");
+const ApiError = require("../utils/ApiError");
+const pick = require("../utils/pick");
+
 const enums = require("../json/enums.json");
-const messages = require("../json/messages.json");
 
-const getUsers = async (req, res) => {
-  try {
-    const users = await userService.getUsers();
-    return res.status(enums.HTTP_CODES.OK).json(users);
-  } catch (error) {
-    logger.error(error.message);
-    return res
-      .status(enums.HTTP_CODES.DUPLICATE_VALUE)
-      .json({ error: error.message });
-  }
-};
+const createUser = catchAsync(async (req, res) => {
+  const user = await userService.createUser(req.body);
+  res.status(enums.HTTP_CODES.OK).send(user);
+});
 
-const get10Users = async (req, res) => {
-  try {
-    const { page, limit } = req.query;
-    const users = await userService.get10Users(page, limit);
-    return res.status(enums.HTTP_CODES.OK).json(users);
-  } catch (error) {
-    logger.error(error.message);
-    return res
-      .status(enums.HTTP_CODES.DUPLICATE_VALUE)
-      .json({ error: error.message });
-  }
-};
+const getUsers = catchAsync(async (req, res) => {
+  const filter = pick(req.query, ["name", "role"]);
+  const options = pick(req.query, ["sortBy", "limit", "page"]);
+  const result = await userService.queryUsers(filter, options);
+  res.send(result);
+});
 
-const getUser = async (req, res) => {
-  try {
-    const user = await userService.getUser(req.user.id);
-    return res.status(enums.HTTP_CODES.OK).json(user);
-  } catch (error) {
-    logger.error(error.message);
-    return res
-      .status(enums.HTTP_CODES.DUPLICATE_VALUE)
-      .json({ error: error.message });
+const getUser = catchAsync(async (req, res) => {
+  const user = await userService.getUserById(req.params.userId);
+  if (!user) {
+    throw new ApiError(enums.HTTP_CODES.NOT_FOUND, "User not found");
   }
-};
+  res.send(user);
+});
 
-const updateUser = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const { userId } = req.params;
-    if (!name || !email || !password) {
-      return res
-        .status(enums.HTTP_CODES.BAD_REQUEST)
-        .json({ message: messages.AUTH_MESSAGES.FILL_DETAILS });
-    }
-    if (userId !== req.user.id) {
-      return res
-        .status(enums.HTTP_CODES.UNAUTHORIZED)
-        .json({ message: messages.AUTH_MESSAGES.UNAUTHORIZED });
-    }
-    const user = await userService.updateUser(
-      req.user.id,
-      name,
-      email,
-      password
-    );
-    return res
-      .status(enums.HTTP_CODES.OK)
-      .json({ message: messages.USER_UPDATED, data: user });
-  } catch (error) {
-    logger.error(error.message);
-    return res
-      .status(enums.HTTP_CODES.DUPLICATE_VALUE)
-      .json({ error: error.message });
-  }
-};
+const updateUser = catchAsync(async (req, res) => {
+  const user = await userService.updateUserById(req.params.userId, req.body);
+  res.send(user);
+});
 
-const deleteUser = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    if (userId !== req.user.id) {
-      return res
-        .status(enums.HTTP_CODES.UNAUTHORIZED)
-        .json({ message: messages.AUTH_MESSAGES.UNAUTHORIZED });
-    }
-    await userService.deleteUser(req.user.id);
-    return res
-      .status(enums.HTTP_CODES.OK)
-      .json({ message: messages.USER_DELETED });
-  } catch (error) {
-    logger.error(error.message);
-    return res
-      .status(enums.HTTP_CODES.DUPLICATE_VALUE)
-      .json({ error: error.message });
-  }
-};
+const deleteUser = catchAsync(async (req, res) => {
+  await userService.deleteUserById(req.params.userId);
+  res.status(enums.HTTP_CODES.NO_CONTENT).send();
+});
 
 module.exports = {
+  createUser,
   getUsers,
-  get10Users,
   getUser,
   updateUser,
   deleteUser
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,25 +3,270 @@ const validate = require("../middleware/validate");
 const userController = require("../controller/user");
 const userValidation = require("../validation/user");
 const authMiddleware = require("../middleware/auth");
-const user = require("../validation/user");
 
 const userRouter = express.Router();
 
-userRouter.get("/", authMiddleware.jwtAuthentication, userController.getUsers);
+userRouter
+  .route("/")
+  .post(
+    authMiddleware.auth("manageUsers"),
+    validate(userValidation.createUser),
+    userController.createUser
+  )
+  .get(
+    authMiddleware.auth("getUsers"),
+    validate(userValidation.getUsers),
+    userController.getUsers
+  );
 
-userRouter.get("/id", authMiddleware.jwtAuthentication, userController.getUser);
-
-userRouter.put(
-  "/:userId",
-  authMiddleware.jwtAuthentication,
-  validate(userValidation.updateProfile),
-  userController.updateUser
-);
-
-userRouter.delete(
-  "/:userId",
-  authMiddleware.jwtAuthentication,
-  userController.deleteUser
-);
+userRouter
+  .route("/:userId")
+  .get(
+    authMiddleware.auth("getUsers"),
+    validate(userValidation.getUser),
+    userController.getUser
+  )
+  .patch(
+    authMiddleware.auth("manageUsers"),
+    validate(userValidation.updateUser),
+    userController.updateUser
+  )
+  .delete(
+    authMiddleware.auth("manageUsers"),
+    validate(userValidation.deleteUser),
+    userController.deleteUser
+  );
 
 module.exports = userRouter;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User management and retrieval
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create a user
+ *     description: Only admins can create other users.
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *               - role
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: must be unique
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 minLength: 8
+ *                 description: At least one number and one letter
+ *               role:
+ *                  type: string
+ *                  enum: [user, admin]
+ *             example:
+ *               name: fake name
+ *               email: fake@example.com
+ *               password: password1
+ *               role: user
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/User'
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateEmail'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *
+ *   get:
+ *     summary: Get all users
+ *     description: Only admins can retrieve all users.
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: name
+ *         schema:
+ *           type: string
+ *         description: User name
+ *       - in: query
+ *         name: role
+ *         schema:
+ *           type: string
+ *         description: User role
+ *       - in: query
+ *         name: sortBy
+ *         schema:
+ *           type: string
+ *         description: sort by query in the form of field:desc/asc (ex. name:asc)
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         default: 10
+ *         description: Maximum number of users
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           default: 1
+ *         description: Page number
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/User'
+ *                 page:
+ *                   type: integer
+ *                   example: 1
+ *                 limit:
+ *                   type: integer
+ *                   example: 10
+ *                 totalPages:
+ *                   type: integer
+ *                   example: 1
+ *                 totalResults:
+ *                   type: integer
+ *                   example: 1
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ */
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     summary: Get a user
+ *     description: Logged in users can fetch only their own user information. Only admins can fetch other users.
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/User'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   patch:
+ *     summary: Update a user
+ *     description: Logged in users can only update their own information. Only admins can update other users.
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: must be unique
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 minLength: 8
+ *                 description: At least one number and one letter
+ *             example:
+ *               name: fake name
+ *               email: fake@example.com
+ *               password: password1
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/User'
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateEmail'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   delete:
+ *     summary: Delete a user
+ *     description: Logged in users can delete only themselves. Only admins can delete other users.
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *     responses:
+ *       "200":
+ *         description: No content
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
